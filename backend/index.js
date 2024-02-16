a65c12ebfa8647739604159110cd525f5321aa12
Soorya Christic M <ultimaterebels2024@gmail.com>
2024-02-16 12:03:48 +0530

    update1

--- backend/index.js
+++ backend/index.js
@@ -14,14 +14,20 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
 
 // Database connection
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "soorya",
-  password: "1234",
-  database: "ecommerse",
-});
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "soorya",
+//   password: "1234",
+//   database: "ecommerse",
+// });
 
 db.connect((err) => {
   if (err) {
